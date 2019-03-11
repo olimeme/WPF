9f75711ddc4716d0132812d6c6e842e7e527bf83
olimeme <alibek.mombekov@mail.ru>
2019-03-11 21:19:52 +0600

    ProgressBar

--- ProgressBarDarkTheme/ProgressBarDarkTheme/MainWindow.xaml.cs
+++ ProgressBarDarkTheme/ProgressBarDarkTheme/MainWindow.xaml.cs
@@ -1,42 +1,55 @@
-﻿using System.ComponentModel;
+﻿using System;
+using System.ComponentModel;
+using System.Threading;
 using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Animation;
+
 namespace ProgressBarDarkTheme
 {
     /// <summary>
     /// Логика взаимодействия для MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window, INotifyPropertyChanged
+    public partial class MainWindow : Window
     {
-        private BackgroundWorker backgroundWorker = new BackgroundWorker();
-
-        private int workerState;
-
-        public event PropertyChangedEventHandler PropertyChanged;
-
-        public int WorkerState
-        {
-            get { return workerState; }
-            set { workerState = value;
-                if (PropertyChanged != null)
-                    PropertyChanged(this, new PropertyChangedEventArgs("WorkerState"));
-                }
-        }
-
         public MainWindow()
         {
             InitializeComponent();
-            DataContext = this;
-            backgroundWorker.DoWork += (s, e) =>
+        }
+
+        private void DoWorkButton_Click(object sender, RoutedEventArgs e)
+        {
+            BackgroundWorker worker = new BackgroundWorker();
+            worker.RunWorkerCompleted += worker_RunWorkerCompleted;
+            worker.WorkerReportsProgress = true;
+            worker.DoWork += worker_DoWork;
+            worker.ProgressChanged += worker_ProgressChanged;
+            worker.RunWorkerAsync();
+        }
+
+        private void worker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            TestProgressBar.Value = e.ProgressPercentage;
+            ProgressTextBlock.Text = (string)e.UserState;
+        }
+
+        private void worker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            var worker = sender as BackgroundWorker;
+            for (int i = 0; i < 10; i++)
             {
-                for (int i = 0; i <= 100; i++)
-                {
-                    System.Threading.Thread.Sleep(100);
-                    WorkerState = i;
-                    progressText = i.ToString() + "%";
-                }
-                MessageBox.Show("Done");
-            };
-            backgroundWorker.RunWorkerAsync();
+                Thread.Sleep(1000);
+                worker.ReportProgress((i + 1) * 10, String.Format("{0}%", (i + 1)*10));
+            }
+
+            worker.ReportProgress(100, "Done Processing.");
+        }
+
+        private void worker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            MessageBox.Show("All Done!");
+            TestProgressBar.Value = 0;
+            ProgressTextBlock.Text = "";
         }
     }
 }
